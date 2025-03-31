352c75a68ccb445c93639b442aaa303086dbc0c8
Joseph Adeleke <32585860+jadeleke@users.noreply.github.com>
2025-03-31 22:55:46 +0000

    added bash scripting and added my whatsapp number to my profile

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const ctx = document.getElementById('skillsChart').getContext('2d');
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Python', 'HTML/CSS', 'JavaScript', 'ML Frameworks', 'Git/GitHub'],
+    labels: ['Python', 'HTML/CSS', 'JavaScript', 'ML Frameworks', 'Bash Scripting', 'Git/GitHub'],
     datasets: [{
       label: 'Proficiency (%)',
-      data: [90, 85, 75, 80, 95],
-      backgroundColor: ['#38bdf8', '#fbbf24', '#ef4444', '#10b981', '#7c3aed'],
+      data: [90, 85, 75, 80, 70, 95],
+      backgroundColor: ['#38bdf8', '#fbbf24', '#ef4444', '#10b981','#5c3aed', '#7c3aed'],
       borderWidth: 1
     }]
   },
