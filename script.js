a21a79bb6f7d7a8aa39aba7b26756b5137ad5694
jadeleke <akdeljoseph@outlook.com>
2025-04-06 02:15:01 +0100

    changed the positions of Ghanavoice to the first one.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const ctx = document.getElementById('skillsChart').getContext('2d');
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Python', 'HTML/CSS', 'JavaScript', 'ML Frameworks', 'Bash Scripting', 'Git/GitHub'],
+    labels: ['Python', 'HTML/CSS', 'JavaScript', 'ML Frameworks', 'Bash Scripting', 'Git/GitHub', 'AWS'],
     datasets: [{
       label: 'Proficiency (%)',
-      data: [90, 85, 75, 80, 70, 95],
+      data: [70, 30, 30, 80, 80, 30],
       backgroundColor: ['#38bdf8', '#fbbf24', '#ef4444', '#10b981','#5c3aed', '#7c3aed'],
       borderWidth: 1
     }]
